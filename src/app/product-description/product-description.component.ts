077b937d137fb0fbb2085ec099166a612bf446a2
unknown <znimed@gmail.com>
2019-11-01 17:08:21 +0100

    Module 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService} from '../product.service'
+import { ProductService} from '../product.service';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -7,7 +8,7 @@ import { ProductService} from '../product.service'
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo : Album;
 
   constructor(private _productService:ProductService) { }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { Album } from './album';
+import {Observable} from 'rxjs/Observable'
 
 import 'rxjs/add/operator/map'
 
@@ -8,9 +10,9 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
+  getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=>
-      response.json()
+      <Album>response.json()
     );
   }
 }

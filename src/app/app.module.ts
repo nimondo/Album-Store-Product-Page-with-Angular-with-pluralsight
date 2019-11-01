88fa01f22c064f879295febd31e0fdc7bc9f953e
unknown <znimed@gmail.com>
2019-11-01 18:02:00 +0100

    Add product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { ProductService } from './product.service'
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Album } from './album';
 import {Observable} from 'rxjs/Observable'
+import { Product } from './product';
 
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
   constructor(private _http: Http) { }
 
   getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=>
       <Album>response.json()
     );
+  }
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=>
+      <Product[]>response.json()
+    ); 
   }
 }
